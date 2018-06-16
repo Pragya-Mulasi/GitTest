8aaeab82d89e42b7142ba92cd7862a5d67a7a1e4
Pragya-Mulasi <Mulasi.pragya16@gmail.com>
2018-06-16 12:58:10 +0100

    First Commit

---
+++ AWSApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AWSApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
